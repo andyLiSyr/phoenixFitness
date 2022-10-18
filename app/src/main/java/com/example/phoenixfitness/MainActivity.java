e4bc380f6d47e18ac2932178381d579a15bdcc92
andyLiSyr <ali142@syr.edu>
2022-10-18 17:34:59 -0400

    Created button functionality for ranking, Created ranking/leaderboard activity and the basic UI.

--- app/src/main/java/com/example/phoenixfitness/MainActivity.java
+++ app/src/main/java/com/example/phoenixfitness/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.phoenixfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button rankingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rankingButton=findViewById(R.id.rankingButton);
+        rankingButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.rankingButton:
+                openRankingActivity();
+                break;
+            default:
+                break;
+        }
+
+    }
+    public void openRankingActivity(){
+        Intent intent= new Intent(this,Ranking.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/phoenixfitness/Ranking.java
@@ -0,0 +1,14 @@
+package com.example.phoenixfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ranking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rankings);
+    }
+}
