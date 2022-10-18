b7ef33d18a6df956b953e2aa7fb90bc9a970ed90
Eric Rodriguez <erodri07@syr.edu>
2022-10-18 19:42:53 -0400

    Made Profile Activity and input cals

--- app/src/main/java/com/example/phoenixfitness/MainActivity.java
+++ app/src/main/java/com/example/phoenixfitness/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button rankingButton;
+    private ImageButton profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         rankingButton=findViewById(R.id.rankingButton);
         rankingButton.setOnClickListener(this);
+        profileButton=findViewById(R.id.profile);
+        profileButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -24,13 +29,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.rankingButton:
                 openRankingActivity();
                 break;
+            case R.id.profile:
+                openProfileActivity();
+                break;
             default:
                 break;
         }
 
     }
+
+    private void openProfileActivity() {
+        Intent intent = new Intent(this,UserProfile.class);
+        startActivity(intent);
+    }
+
     public void openRankingActivity(){
         Intent intent= new Intent(this,Ranking.class);
         startActivity(intent);
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/phoenixfitness/UserProfile.java
@@ -0,0 +1,14 @@
+package com.example.phoenixfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class UserProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userprofile);
+    }
+}
