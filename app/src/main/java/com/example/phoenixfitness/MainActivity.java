3314d4896b45005fd2e5995a467eaad0f29de52c
Petr Sopidi <79230185+psopidi@users.noreply.github.com>
2022-10-19 16:34:49 -0400

    Fixed Icon Issues
    Fixed Issues

--- app/src/main/java/com/example/phoenixfitness/MainActivity.java
+++ app/src/main/java/com/example/phoenixfitness/MainActivity.java
@@ -7,24 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button rankingButton;
     private ImageButton profileButton;
-    private Button friendsOnline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rankingButton = findViewById(R.id.rankingButton);
+        rankingButton=findViewById(R.id.rankingButton);
         rankingButton.setOnClickListener(this);
-        profileButton = findViewById(R.id.profile);
+        profileButton=findViewById(R.id.profile);
         profileButton.setOnClickListener(this);
-        friendsOnline = findViewById(R.id.friends);
-        friendsOnline.setOnClickListener(this);
-
 
     }
 
@@ -36,10 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.profile:
                 openProfileActivity();
-                break;
-            case R.id.friends:
-                openFriendsActivity();
-                Toast.makeText(this,"Friends Are TextView for now", Toast.LENGTH_LONG).show();
                 break;
             default:
                 break;
@@ -53,11 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void openRankingActivity(){
-        Intent intent = new Intent(this,Ranking.class);
-        startActivity(intent);
-    }
-    public void openFriendsActivity(){
-        Intent intent = new Intent(this,Friends.class);
+        Intent intent= new Intent(this,Ranking.class);
         startActivity(intent);
     }
 
